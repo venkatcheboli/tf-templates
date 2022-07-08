1b80465f63d3d55c1afbbdea7a3170e5c1b8d959
Venkata Cheboli <venkat.cheboli@gmail.com>
2022-07-08 17:38:56 -0400

    Update ec2-instance.tf
    Updated resource Helloword1 , Helloworld2

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,14 +1,30 @@
 
 resource "aws_instance" "web" {
-  #ami           = data.aws_ami.ubuntu.id
-   # ami           = "ami-02f3416038bdb17fb"
    ami = var.ami
    instance_type = var.instance_type
    subnet_id= var.subnet_id
-
-  #instance_type = "t2.micro"
-
+   security_groups=["sg-02bc00f0a8d9e8e31"]
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web" {
+   ami = var.ami
+   instance_type = var.instance_type
+   subnet_id= var.subnet_id
+   security_groups=["sg-02bc00f0a8d9e8e31"]
+  tags = {
+    Name = "HelloWorld1"
+  }
+}
+
+resource "aws_instance" "web" {
+   ami = var.ami
+   instance_type = var.instance_type
+   subnet_id= var.subnet_id
+   security_groups=["sg-02bc00f0a8d9e8e31"]
+  tags = {
+    Name = "HelloWorld2"
   }
 }
