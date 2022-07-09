bd773d8748a540012fd54bec42fae22467cf0c58
Venkata Cheboli <venkat.cheboli@gmail.com>
2022-07-08 21:48:54 -0400

    Update variables.tf
    instance type , there is unnecessary gap between instance and type

--- variables.tf
+++ variables.tf
@@ -2,7 +2,10 @@ variable "ami" {
 
     default= "ami-02f3416038bdb17fb"
 }
-variable "instance _type" {
+variable "instance_type" {
 
     default= "t2.micro"
+}
+variable "subnet_id"{
+    default= "vpc-0a3573fc65b69c4fb"
 }
